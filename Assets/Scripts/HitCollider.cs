f7e96c1e2bfa2f92351e410fed979e2ead9e0e2d
okday13123 <okday13123@gmail.com>
2018-01-27 00:49:28 +0700

    implement hitcontroller and tagmanager

---
+++ Assets/Scripts/HitCollider.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitCollider : MonoBehaviour {
+
+	public enum OnTriggeris {Enter,Exit,Stay};
+	public OnTriggeris step = OnTriggeris.Enter;
+	public string tagofObj;
+	public float counttime;
+	private float forpickup;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		//In 
+		tagofObj = other.tag;
+		forpickup = counttime;
+		step = OnTriggeris.Enter;
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		//Out
+
+		step = OnTriggeris.Exit;
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		//Stay
+
+		step = OnTriggeris.Stay;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (step == OnTriggeris.Stay) {
+			if (forpickup >= 0) {
+				forpickup -= Time.deltaTime;
+			} else {
+				if(tagofObj == "people")
+				Debug.Log ("pickup");
+				else if(tagofObj == "building")
+					Debug.Log ("sendpeople");
+			}
+		}
+	}
+}
