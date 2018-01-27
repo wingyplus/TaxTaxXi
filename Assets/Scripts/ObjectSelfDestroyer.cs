6e9d690d6e2875491e42aa0fd624f098432d446a
pugkung <tanapol.raa@gmail.com>
2018-01-27 11:26:04 +0700

    implement objectspawner and add road spawner

---
+++ Assets/Scripts/ObjectSelfDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSelfDestroyer : MonoBehaviour {
+
+    public GameObject objectDestructionpoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        objectDestructionpoint = GameObject.Find("ObjectDestructionCheck");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > objectDestructionpoint.transform.position.x)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    public Transform generationpoint;
+    public float distance;
+
+    public float distancemin;
+    public float distancemax;
+
+    private int selector;
+    private int startselector;
+    private float[] objectswidth;
+
+    public ObjectPooler[] ObjectPool;
+
+    void Start()
+    {
+        objectswidth = new float[ObjectPool.Length];
+        for (int i = 0; i < ObjectPool.Length; i++)
+        {
+            objectswidth[i] = ObjectPool[i].pooledObject.GetComponent<SpriteRenderer>().bounds.size.x;
+        }
+    }
+
+    void Update()
+    {
+        if (transform.position.x + Screen.width/20 > generationpoint.position.x)
+        {
+            startselector = Random.Range(0, ObjectPool.Length);
+            selector = Random.Range(0, ObjectPool.Length);
+            distance = Random.Range(distancemin, distancemax);
+
+            GameObject startPlatform = ObjectPool[startselector].GetPooledObject();
+            transform.position = new Vector3(transform.position.x - (objectswidth[startselector] / 2) - distance, transform.position.y, transform.position.z);
+            startPlatform.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            startPlatform.transform.rotation = transform.rotation;
+            startPlatform.SetActive(true);
+            transform.position = new Vector3(transform.position.x - (objectswidth[selector] / 2), transform.position.y, transform.position.z);
+        }
+    }
+}
