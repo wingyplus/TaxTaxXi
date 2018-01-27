746d6bd4b1762cddf342bec3679bea80119685ca
Metalbuster <jankla2010@gmail.com>
2018-01-27 19:33:46 +0700

    i merge

---
+++ Assets/Scripts/ResultScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultScreen : MonoBehaviour {
+
+	public string menu;
+
+	public void RestartGame(){
+		
+	}
+
+	public void QuitTomenu(){
+		SceneManager.LoadScene(menu);
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public Text screentime;
+	public Text screentime1;
+	public float starttime;
+	public float time;
+	public float countdown;
+	public bool count;
+
+
+	// Use this for initialization
+	void Start () {
+		time = starttime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(count){
+		time -= countdown * Time.deltaTime;
+		screentime.text = "Timer : " + Mathf.Round (time);
+		screentime1.text = "Timer : " + Mathf.Round (time);
+		}
+
+		if (time <= 0) {
+			count = false;
+		}
+	}
+}
