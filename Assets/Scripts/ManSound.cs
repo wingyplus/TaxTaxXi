d486906a2bd4cc68d0ae104b63cae7a71ec7d83d
Prayoch Rujira <prayoch@clockupstudio.com>
2018-01-27 16:07:31 +0700

    Add ManSound script

---
+++ Assets/Scripts/ManSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ManSound : MonoBehaviour {
+
+	public AudioClip pickUpSound;
+	public AudioClip sentSound;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "car" && (other.GetComponent<Rigidbody2D>().velocity.x == 0)) {
+			PlayPickUpSound();
+		}
+		
+	}
+
+	public void PlayPickUpSound() {
+		GetComponent<AudioSource>().PlayOneShot(pickUpSound);
+	}
+
+	public void PlaySentSound() {
+		GetComponent<AudioSource>().PlayOneShot(sentSound);
+	}
+}
